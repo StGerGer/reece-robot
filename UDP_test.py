29a123b2056e1ab20689b123859967a57dee42cb
Nate St. George <nateestes223@gmail.com>
2016-12-12 08:16:57 -0700

    Everything is good now

--- UDP_test.py
+++ UDP_test.py
@@ -3,6 +3,18 @@ from __future__ import print_function
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+from pyfirmata import Arduino, util
+
+board = Arduino('/dev/ttyUSB0')     # If it doesn't work, try changing this number
+
+leftSpeedPin = 5
+leftForwardPin = 8
+leftBackwardPin = 7
+
+rightSpeedPin = 6
+rightForwardPin = 4
+rightBackwardPin = 9
+
 # Data: 0                    0                    0                    0                    000              000 (no spaces)
 #       ^ R Motor Direction  ^ R Motor Direction  ^ L Motor Direction  ^ L Motor Direction  ^ R Motor Speed  ^ L Motor Speed
 # Examples: 1001255255 Would be a full right (maybe left) turn, 1010255255 full speed forward, 1010100100 slower forward
@@ -12,6 +24,15 @@ class Echo(DatagramProtocol):
 
     def datagramReceived(self, data, addr):
         print("received %r from %s" % (data, addr))
+
+        board.digital[leftForwardPin].write(data[2])
+        board.digital[leftBackwardPin].write(data[3])
+        board.pwm[leftSpeedPin].write(data[4:6]/255 || 1)
+        
+        board.digital[rightForwardPin].write(data[0])
+        board.digital[rightBackwardPin].write(data[1])
+        board.pwm[rightSpeedPin].write(data[7:9]/255 || 1)
+                
         self.transport.write(data, addr)
 
 reactor.listenUDP(9999, Echo())
--- node_sender.js
+++ node_sender.js
@@ -1,35 +1,36 @@
+// Note: In order for this to work properly, the xevEmitter is linux-only.
+// There may be a way for this to work on Windows (since 10 now has bash), but I haven't tested it.
+
 const dgram = require('dgram');
 
 const xevEmitter = require('xev-emitter')(process.stdin)
 
-const client = dgram.createSocket('udp4');
-const port = 9989;
-const host = '10.80.49.162';
+const client = dgram.createSocket('udp4'); // Creates a UDP socket for sending packets
+const port = 9989; // Port that the robot's RPi is listening on
+const host = '10.80.49.162'; // IP of the RPi
 
 var messageString;
 var message;
 
-xevEmitter.on('KeyPress', function(key) {
-    console.log(key+" was pressed");
+xevEmitter.on('KeyPress', function(key) { // Runs on keypress
     if(key == 'w') {
-        messageString = '1010255255';
+        messageString = '1010255255';   // 1010 = forward, 255255 = full speed
     } else if (key == 's') {
-        messageString = '0101255255';
+        messageString = '0101255255';   // 0101 = backward
     } else if (key == 'a') {
-        messageString = '1001255255';
+        messageString = '1001255255';   // 1001 = left
     } else if (key == 'd') {
-        messageString = '0110255255';
+        messageString = '0110255255';   // 0110 = right
     } else if (key == 'space') {
-        console.log('stop');
-        messageString = '0000255255';
+        messageString = '0000255255';   // 0000 = stop
     }
 
-    message = new Buffer(messageString);
-    client.send(message, 0, message.length, port, host, function (err, bytes) {});
+    message = new Buffer(messageString); // Make the message part of the packet
+    client.send(message, 0, message.length, port, host, function (err, bytes) {}); // Send packet
 });
 
-xevEmitter.on('KeyRelease', function(key) {
-    messageString = '0000255255';
+xevEmitter.on('KeyRelease', function(key) { // When the key is no longer being pressed
+    messageString = '0000255255';           // Make it stop
     message = new Buffer(messageString);
     client.send(message, 0, message.length, port, host, function (err, bytes) {});
 });
